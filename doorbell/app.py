8faf693eca09915b7cfb032d621adba39c93a79d
Chris Hutchinson <chris_hutchinson@me.com>
2017-08-08 09:15:42 +0100

    Initial commit

---
+++ doorbell/app.py
@@ -0,0 +1,33 @@
+import RPi.GPIO as GPIO
+import time
+import requests
+import yaml
+
+config = yaml.safe_load(open("config.yml"))
+
+GPIO.setmode(GPIO.BOARD)
+
+buttonPin = 11
+ledPin = 16
+
+GPIO.setup(buttonPin, GPIO.IN)
+GPIO.setup(ledPin, GPIO.OUT)
+
+GPIO.output(ledPin, GPIO.LOW)
+
+# Request details
+url = config.makerUrl
+
+print "Listening for doorbell presses..."
+try:
+  while 1:
+    if GPIO.input(buttonPin):
+      GPIO.output(ledPin, GPIO.LOW)
+    else:
+      GPIO.output(ledPin, GPIO.HIGH)
+      requests.post(url)
+      print "On"
+
+    time.sleep(0.1)
+except KeyboardInterrupt:
+  GPIO.cleanup()
